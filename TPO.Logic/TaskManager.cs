36c60b334f46422e147d24e5e5c8556afe439e7e
Klishin Ilya <klishin-ilya@mail.ru>
2021-01-03 20:48:09 +0300

    Initial logic classes

---
+++ TPO.Logic/TaskManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPO.Logic
+{
+    /// <summary>
+    /// Менеджер задач
+    /// </summary>
+    public class TaskManager
+    {
+        
+        private IList<UserTask> _tasks = new List<UserTask>();
+    }
+}
---
+++ TPO.Logic/UserTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPO.Logic
+{
+    /// <summary>
+    /// Сущность задачи
+    /// </summary>
+    public struct Task
+    {
+        public string Name { set; get; }
+        public string Description { set; get; }
+    }
+}
